b490b1e20b6250966ae6128dc5462849bd6dcd22
NamHayCode <phanhoainam.work@gmail.com>
2024-06-04 11:05:57 +0700

    lession3

---
+++ les3/router/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Respond to GET request at the root
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Respond to POST request at the root
+app.post('/', (req, res) => {
+  res.send('Got a POST request');
+});
+
+// Respond to PUT request at /user
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user');
+});
+
+// Respond to DELETE request at /user
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
